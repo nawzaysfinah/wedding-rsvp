1fac7884a0599cad5c5081b0902549b7a8186be2
Syaz <nawzaysfinah@gmail.com>
2025-07-29 21:02:44 +0800

    moved schedule up

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -6,21 +6,13 @@ const Schedule = () => {
     <>
       <section
         id="schedule"
-        className="h-screen w-full px-4 text-[#4a4a4a] flex items-center justify-center pt-12"
+        className="h-screen w-full px-4 text-[#4a4a4a] flex items-center justify-center pt-12 -translate-y-[10vh]"
       >
         <div className="w-full">
           <div className="max-w-4xl mx-auto">
-            {/* <h2 className="text-3xl font-bold font-serif mb-8 text-center">
-              25<sup>th</sup> October 2025
-            </h2> */}
-
             <div className="space-y-6">
-              <div>
-                <h3 className="text-xl font-semibold font-serif">
-                  Tea Ceremony
-                </h3>
-                <p className="text-gray-700">10:30 AM – 11:30 AM</p>
-              </div>
+              <h3 className="text-xl font-semibold font-serif">Tea Ceremony</h3>
+              <p className="text-gray-700">10:30 AM – 11:30 AM</p>
               <br></br>
               <div>
                 <h3 className="text-xl font-semibold font-serif">
